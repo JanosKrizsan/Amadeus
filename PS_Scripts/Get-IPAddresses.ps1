2103a6624b9b659d8448d985433ee815a25b7b66
John Kriz <janoskrizsan402@gmail.com>
2020-06-04 22:42:08 +0200

    lame commit, I should read less and do more

---
+++ PS_Scripts/Get-IPAddresses.ps1
@@ -0,0 +1,26 @@
+<#
+.Synopsis
+    Gets IP addresses for the selected adapter.
+.Description
+    Gets adapter's IPV6 and IPV4 addresses (if applicable)
+.Notes
+    Gets adapter parameter
+    Gets IP addresses associated with adapter.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [object]
+    $NetAdapter
+)
+
+$OSInfo = (Get-WmiObject Win32_OperatingSystem).Name 
+$IsNewerOs = $OsInfo -match '8' -or $OsInfo -match '10'
+if ($IsNewerOs) {
+    $IPs = $NetAdapter | Get-NetIPAddress | Select-Object -Property IPAddress | ForEach-Object {"$($_.IPAddress)"}
+} else {
+    $NetAdapter = $NetAdapter.ifDesc
+    $IPs = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "Description = '$NetAdapter'"
+}
+
+return $IPs
