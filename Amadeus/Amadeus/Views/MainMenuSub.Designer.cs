3c09b1bee04697b9ad086bff99829787d2a24906
John Kriz <janoskrizsan402@gmail.com>
2020-06-16 13:43:25 +0200

    language settings panel

--- Amadeus/Amadeus/Views/MainMenuSub.Designer.cs
+++ Amadeus/Amadeus/Views/MainMenuSub.Designer.cs
@@ -29,26 +29,28 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.removeSelectedBtn = new System.Windows.Forms.Button();
+            this.stopCurrentBtn = new System.Windows.Forms.Button();
+            this.runCustomBtn = new System.Windows.Forms.Button();
+            this.createNewBtn = new System.Windows.Forms.Button();
+            this.scriptLbl = new System.Windows.Forms.Label();
+            this.saveAsCsvBtn = new System.Windows.Forms.Button();
+            this.saveAsXmlBtn = new System.Windows.Forms.Button();
+            this.resetCrntBtn = new System.Windows.Forms.Button();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.reportingLbl = new System.Windows.Forms.Label();
+            this.saveAsTxtBtn = new System.Windows.Forms.Button();
             this.currentStatusBar = new System.Windows.Forms.ProgressBar();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.saveAsTxtBtn = new System.Windows.Forms.Button();
             this.procStatusLbl = new System.Windows.Forms.Label();
-            this.reportingLbl = new System.Windows.Forms.Label();
-            this.openFileBtn = new System.Windows.Forms.Button();
-            this.resetCrntBtn = new System.Windows.Forms.Button();
-            this.saveAsXmlBtn = new System.Windows.Forms.Button();
-            this.saveAsCsvBtn = new System.Windows.Forms.Button();
-            this.scriptLbl = new System.Windows.Forms.Label();
-            this.createNewBtn = new System.Windows.Forms.Button();
-            this.runCustomBtn = new System.Windows.Forms.Button();
-            this.stopCurrentBtn = new System.Windows.Forms.Button();
-            this.removeSelectedBtn = new System.Windows.Forms.Button();
+            this.menuSubHelpBtn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.menuSubHelpBtn);
             this.panel1.Controls.Add(this.removeSelectedBtn);
             this.panel1.Controls.Add(this.stopCurrentBtn);
             this.panel1.Controls.Add(this.runCustomBtn);
@@ -64,6 +66,108 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(592, 202);
             this.panel1.TabIndex = 0;
+            // 
+            // removeSelectedBtn
+            // 
+            this.removeSelectedBtn.Location = new System.Drawing.Point(98, 142);
+            this.removeSelectedBtn.Name = "removeSelectedBtn";
+            this.removeSelectedBtn.Size = new System.Drawing.Size(104, 23);
+            this.removeSelectedBtn.TabIndex = 10;
+            this.removeSelectedBtn.Text = "Remove Selected";
+            this.removeSelectedBtn.UseVisualStyleBackColor = true;
+            // 
+            // stopCurrentBtn
+            // 
+            this.stopCurrentBtn.Location = new System.Drawing.Point(297, 142);
+            this.stopCurrentBtn.Name = "stopCurrentBtn";
+            this.stopCurrentBtn.Size = new System.Drawing.Size(83, 23);
+            this.stopCurrentBtn.TabIndex = 9;
+            this.stopCurrentBtn.Text = "Stop Current";
+            this.stopCurrentBtn.UseVisualStyleBackColor = true;
+            // 
+            // runCustomBtn
+            // 
+            this.runCustomBtn.Location = new System.Drawing.Point(208, 142);
+            this.runCustomBtn.Name = "runCustomBtn";
+            this.runCustomBtn.Size = new System.Drawing.Size(83, 23);
+            this.runCustomBtn.TabIndex = 8;
+            this.runCustomBtn.Text = "Run Custom";
+            this.runCustomBtn.UseVisualStyleBackColor = true;
+            // 
+            // createNewBtn
+            // 
+            this.createNewBtn.Location = new System.Drawing.Point(6, 142);
+            this.createNewBtn.Name = "createNewBtn";
+            this.createNewBtn.Size = new System.Drawing.Size(83, 23);
+            this.createNewBtn.TabIndex = 7;
+            this.createNewBtn.Text = "Add New";
+            this.createNewBtn.UseVisualStyleBackColor = true;
+            // 
+            // scriptLbl
+            // 
+            this.scriptLbl.AutoSize = true;
+            this.scriptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scriptLbl.Location = new System.Drawing.Point(3, 101);
+            this.scriptLbl.Name = "scriptLbl";
+            this.scriptLbl.Size = new System.Drawing.Size(107, 17);
+            this.scriptLbl.TabIndex = 6;
+            this.scriptLbl.Text = "Script / Process";
+            // 
+            // saveAsCsvBtn
+            // 
+            this.saveAsCsvBtn.Location = new System.Drawing.Point(187, 41);
+            this.saveAsCsvBtn.Name = "saveAsCsvBtn";
+            this.saveAsCsvBtn.Size = new System.Drawing.Size(83, 23);
+            this.saveAsCsvBtn.TabIndex = 5;
+            this.saveAsCsvBtn.Text = "Save As CSV";
+            this.saveAsCsvBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveAsXmlBtn
+            // 
+            this.saveAsXmlBtn.Location = new System.Drawing.Point(98, 41);
+            this.saveAsXmlBtn.Name = "saveAsXmlBtn";
+            this.saveAsXmlBtn.Size = new System.Drawing.Size(83, 23);
+            this.saveAsXmlBtn.TabIndex = 4;
+            this.saveAsXmlBtn.Text = "Save As XML";
+            this.saveAsXmlBtn.UseVisualStyleBackColor = true;
+            // 
+            // resetCrntBtn
+            // 
+            this.resetCrntBtn.Location = new System.Drawing.Point(357, 41);
+            this.resetCrntBtn.Name = "resetCrntBtn";
+            this.resetCrntBtn.Size = new System.Drawing.Size(83, 23);
+            this.resetCrntBtn.TabIndex = 3;
+            this.resetCrntBtn.Text = "Reset Current";
+            this.resetCrntBtn.UseVisualStyleBackColor = true;
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(276, 41);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.openFileBtn.TabIndex = 2;
+            this.openFileBtn.Text = "Open File";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // reportingLbl
+            // 
+            this.reportingLbl.AutoSize = true;
+            this.reportingLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportingLbl.Location = new System.Drawing.Point(3, 0);
+            this.reportingLbl.Name = "reportingLbl";
+            this.reportingLbl.Size = new System.Drawing.Size(70, 17);
+            this.reportingLbl.TabIndex = 1;
+            this.reportingLbl.Text = "Reporting";
+            // 
+            // saveAsTxtBtn
+            // 
+            this.saveAsTxtBtn.Location = new System.Drawing.Point(3, 41);
+            this.saveAsTxtBtn.Name = "saveAsTxtBtn";
+            this.saveAsTxtBtn.Size = new System.Drawing.Size(86, 23);
+            this.saveAsTxtBtn.TabIndex = 0;
+            this.saveAsTxtBtn.Text = "Save as Text";
+            this.saveAsTxtBtn.UseVisualStyleBackColor = true;
+            this.saveAsTxtBtn.Click += new System.EventHandler(this.saveToFileBtn_Click);
             // 
             // currentStatusBar
             // 
@@ -84,16 +188,6 @@
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
-            // saveAsTxtBtn
-            // 
-            this.saveAsTxtBtn.Location = new System.Drawing.Point(6, 51);
-            this.saveAsTxtBtn.Name = "saveAsTxtBtn";
-            this.saveAsTxtBtn.Size = new System.Drawing.Size(86, 23);
-            this.saveAsTxtBtn.TabIndex = 0;
-            this.saveAsTxtBtn.Text = "Save as Text";
-            this.saveAsTxtBtn.UseVisualStyleBackColor = true;
-            this.saveAsTxtBtn.Click += new System.EventHandler(this.saveToFileBtn_Click);
-            // 
             // procStatusLbl
             // 
             this.procStatusLbl.AutoSize = true;
@@ -105,97 +199,14 @@
             this.procStatusLbl.TabIndex = 3;
             this.procStatusLbl.Text = "Idle";
             // 
-            // reportingLbl
+            // menuSubHelpBtn
             // 
-            this.reportingLbl.AutoSize = true;
-            this.reportingLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.reportingLbl.Location = new System.Drawing.Point(3, 0);
-            this.reportingLbl.Name = "reportingLbl";
-            this.reportingLbl.Size = new System.Drawing.Size(70, 17);
-            this.reportingLbl.TabIndex = 1;
-            this.reportingLbl.Text = "Reporting";
-            // 
-            // openFileBtn
-            // 
-            this.openFileBtn.Location = new System.Drawing.Point(276, 51);
-            this.openFileBtn.Name = "openFileBtn";
-            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
-            this.openFileBtn.TabIndex = 2;
-            this.openFileBtn.Text = "Open File";
-            this.openFileBtn.UseVisualStyleBackColor = true;
-            // 
-            // resetCrntBtn
-            // 
-            this.resetCrntBtn.Location = new System.Drawing.Point(357, 51);
-            this.resetCrntBtn.Name = "resetCrntBtn";
-            this.resetCrntBtn.Size = new System.Drawing.Size(83, 23);
-            this.resetCrntBtn.TabIndex = 3;
-            this.resetCrntBtn.Text = "Reset Current";
-            this.resetCrntBtn.UseVisualStyleBackColor = true;
-            // 
-            // saveAsXmlBtn
-            // 
-            this.saveAsXmlBtn.Location = new System.Drawing.Point(98, 51);
-            this.saveAsXmlBtn.Name = "saveAsXmlBtn";
-            this.saveAsXmlBtn.Size = new System.Drawing.Size(83, 23);
-            this.saveAsXmlBtn.TabIndex = 4;
-            this.saveAsXmlBtn.Text = "Save As XML";
-            this.saveAsXmlBtn.UseVisualStyleBackColor = true;
-            // 
-            // saveAsCsvBtn
-            // 
-            this.saveAsCsvBtn.Location = new System.Drawing.Point(187, 51);
-            this.saveAsCsvBtn.Name = "saveAsCsvBtn";
-            this.saveAsCsvBtn.Size = new System.Drawing.Size(83, 23);
-            this.saveAsCsvBtn.TabIndex = 5;
-            this.saveAsCsvBtn.Text = "Save As CSV";
-            this.saveAsCsvBtn.UseVisualStyleBackColor = true;
-            // 
-            // scriptLbl
-            // 
-            this.scriptLbl.AutoSize = true;
-            this.scriptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scriptLbl.Location = new System.Drawing.Point(3, 101);
-            this.scriptLbl.Name = "scriptLbl";
-            this.scriptLbl.Size = new System.Drawing.Size(107, 17);
-            this.scriptLbl.TabIndex = 6;
-            this.scriptLbl.Text = "Script / Process";
-            // 
-            // createNewBtn
-            // 
-            this.createNewBtn.Location = new System.Drawing.Point(6, 142);
-            this.createNewBtn.Name = "createNewBtn";
-            this.createNewBtn.Size = new System.Drawing.Size(83, 23);
-            this.createNewBtn.TabIndex = 7;
-            this.createNewBtn.Text = "Add New";
-            this.createNewBtn.UseVisualStyleBackColor = true;
-            // 
-            // runCustomBtn
-            // 
-            this.runCustomBtn.Location = new System.Drawing.Point(208, 142);
-            this.runCustomBtn.Name = "runCustomBtn";
-            this.runCustomBtn.Size = new System.Drawing.Size(83, 23);
-            this.runCustomBtn.TabIndex = 8;
-            this.runCustomBtn.Text = "Run Custom";
-            this.runCustomBtn.UseVisualStyleBackColor = true;
-            // 
-            // stopCurrentBtn
-            // 
-            this.stopCurrentBtn.Location = new System.Drawing.Point(297, 142);
-            this.stopCurrentBtn.Name = "stopCurrentBtn";
-            this.stopCurrentBtn.Size = new System.Drawing.Size(83, 23);
-            this.stopCurrentBtn.TabIndex = 9;
-            this.stopCurrentBtn.Text = "Stop Current";
-            this.stopCurrentBtn.UseVisualStyleBackColor = true;
-            // 
-            // removeSelectedBtn
-            // 
-            this.removeSelectedBtn.Location = new System.Drawing.Point(98, 142);
-            this.removeSelectedBtn.Name = "removeSelectedBtn";
-            this.removeSelectedBtn.Size = new System.Drawing.Size(104, 23);
-            this.removeSelectedBtn.TabIndex = 10;
-            this.removeSelectedBtn.Text = "Remove Selected";
-            this.removeSelectedBtn.UseVisualStyleBackColor = true;
+            this.menuSubHelpBtn.Location = new System.Drawing.Point(502, 172);
+            this.menuSubHelpBtn.Name = "menuSubHelpBtn";
+            this.menuSubHelpBtn.Size = new System.Drawing.Size(83, 23);
+            this.menuSubHelpBtn.TabIndex = 11;
+            this.menuSubHelpBtn.Text = "Help!";
+            this.menuSubHelpBtn.UseVisualStyleBackColor = true;
             // 
             // MainMenuSub
             // 
@@ -235,5 +246,6 @@
         private System.Windows.Forms.Button stopCurrentBtn;
         private System.Windows.Forms.Button runCustomBtn;
         private System.Windows.Forms.Button createNewBtn;
+        private System.Windows.Forms.Button menuSubHelpBtn;
     }
 }
